6dcd0a3f1240c9f1dd8d39a2a44d644c6a3334cd
Damian Gamboa <damianponz280792@gmail.com>
2026-04-12 16:57:15 -0600

    feature: install and configure CORS for frontend integration

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require ('express')
+const cors = require('cors')
 const app = express()
 
+// habilita CORS para cualquier origen
+app.use(cors())
+
+// middleware, intercepta requests en formato JSON y los convierte en un obj de JS
 app.use(express.json())
 
+// prefijo de todas las rutas
 const museumsRoutes = require('./routes/museums.routes');
 app.use('/api/museums', museumsRoutes);
 
