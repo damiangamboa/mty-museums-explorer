b067a1ff51ec1abce924a7a8d08d34cc1d879c1a
Damian Gamboa <damianponz280792@gmail.com>
2026-04-12 19:52:40 -0600

    fix(backend): museum images and server asset paths

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const cors = require('cors')
+const path = require('path');
 const app = express()
 
 // habilita CORS para cualquier origen
@@ -7,6 +8,8 @@ app.use(cors())
 
 // middleware, intercepta requests en formato JSON y los convierte en un obj de JS
 app.use(express.json())
+
+app.use('/assets', express.static(path.join(__dirname, '..', '..', 'client', 'assets')));
 
 // prefijo de todas las rutas
 const museumsRoutes = require('./routes/museums.routes');
